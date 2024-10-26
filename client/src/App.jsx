15b5754ce6052aab7702a7160242a153864d495b
Jose Eduardo Payamps <josenxs12@gmail.com>
2024-10-26 17:41:22 -0400

    Add Dashboard route to App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,13 +4,15 @@ import { Routes, Route } from "react-router-dom";
 //pages
 import Landing from "./Pages/Landing";
 import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="login" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </div>
   );
