d2cd53f851a5a0cd87283e0206b23009c802ea5a
BerryRahul <113147217+BerryRahul@users.noreply.github.com>
2024-10-27 07:49:13 -0400

    fixed sidebar margins

--- client/src/Components/DashboardComponents/SideBar.jsx
+++ client/src/Components/DashboardComponents/SideBar.jsx
@@ -4,20 +4,17 @@ const SideBar = () => {
   return (
     <div className="flex h-screen">
       <div className="flex flex-col bg-white h-full w-60 overflow-y-auto fixed">
-        <ul className="space-y-2">
-          <TowerObject name="Tower 1" />
-          <TowerObject name="Tower 2" />
-          <TowerObject name="Tower 3" />
-          <TowerObject name="Tower 4" />
-          <TowerObject name="Tower 5" />
-          <TowerObject name="Tower 6" />
-          <TowerObject name="Tower 7" />
-          <TowerObject name="Tower 8" />
-          <TowerObject name="Tower 9" />
-          <TowerObject name="Tower 10" />
-          <TowerObject name="Tower 11" />
-          <TowerObject name="Tower 12" />
-          <TowerObject name="Tower 13" />
+        <ul className="space-y-2 mt-20">
+          <TowerObject name="Tower A" />
+          <TowerObject name="Tower B" />
+          <TowerObject name="Tower C" />
+          <TowerObject name="Tower D" />
+          <TowerObject name="Tower E" />
+          <TowerObject name="Tower F" />
+          <TowerObject name="Tower G" />
+          <TowerObject name="Tower H" />
+          <TowerObject name="Tower I" />
+          <TowerObject name="Tower J" />
         </ul>
       </div>
       <div className="flex-grow ml-60"> </div>
